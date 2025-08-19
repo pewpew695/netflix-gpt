13fa352643b14ae6ccc63b618a7e38d085524093
pewpew695 <ashpd02@gmail.com>
2025-08-19 21:24:57 +0530

    Removed Confidential keys

--- src/utils/openAI.jsx
+++ src/utils/openAI.jsx
@@ -1,5 +1,4 @@
-const API_KEY =
-  "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"; // Replace with your OpenAI API key
+const API_KEY = ""; // Replace with your OpenAI API key
 const ENDPOINT = "https://api.openai.com/v1/responses";
 
 async function getResponse(userMessage) {
